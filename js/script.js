35108bb2b1595f3d140c543312a7cabb603564c8
Simone <simone.sichili@gmail.com>
2021-01-14 16:27:44 +0100

    Add dynamic elements in "gallery", "process" and "blog" .

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            icona: "<i class='fab fa-twitter'></i>",
             currentNavIndex: 0,
             navElements: [
                 {
@@ -30,6 +29,96 @@ var app = new Vue(
                     text: "Elements"
                 },
             ],
+            galleryCards: [
+                {
+                    imgUrl: "img/h-2-port-img-1.jpg",
+                    title: "All these illustrations",
+                    subtitle: "Illustration"
+                },
+                {
+                    imgUrl: "img/h-2-port-img-2.jpg",
+                    title: "Type and illustrations",
+                    subtitle: "Illustration"
+                },
+                {
+                    imgUrl: "img/h-2-port-img-3.jpg",
+                    title: "Draw and illustrate",
+                    subtitle: "Illustration"
+                },
+                {
+                    imgUrl: "img/h-2-port-img-4.jpg",
+                    title: "Illustrations of novels",
+                    subtitle: "Illustration"
+                },
+                {
+                    imgUrl: "img/h-2-port-img-5.jpg",
+                    title: "Dreams are illustrations",
+                    subtitle: "Illustration"
+                },
+                {
+                    imgUrl: "img/h-2-port-img-6.jpg",
+                    title: "Illustration of novels",
+                    subtitle: "Illustration"
+                },
+            ],
+            processElements: [
+                {
+                    icon: "fas fa-lightbulb",
+                    title: "First theres an idea",
+                    paragraph: "Lorem ipsum dolor sit, conetu adipisc sed be et ore aliqua pleasure itself."
+                },
+                {
+                    icon: "far fa-comment",
+                    title: "Then we talk about",
+                    paragraph: "Lorem ipsum dolor sit, conetu adipisc sed be et ore aliqua pleasure itself."
+                },
+                {
+                    icon: "fas fa-cloud",
+                    title: "And we think about",
+                    paragraph: "Lorem ipsum dolor sit, conetu adipisc sed be et ore aliqua pleasure itself."
+                },
+                {
+                    icon: "fas fa-pencil-ruler",
+                    title: "So we draw along",
+                    paragraph: "Lorem ipsum dolor sit, conetu adipisc sed be et ore aliqua pleasure itself."
+                },
+            ],
+            blogArticles:  [
+                {
+                    dateTop: "07",
+                    dateBottom: "MAY '21",
+                    url: "#",
+                    imgUrl: "img/h-2-blog-img-1.jpg",
+                    title: "This way, loves: a detailed guide through new design",
+                    author: "By Emily Fields"
+                },
+                {
+                    dateTop: "07",
+                    dateBottom: "MAY '21",
+                    url: "#",
+                    imgUrl: "img/h-2-blog-img-2.jpg",
+                    title: "I try to give people a different way of looking art",
+                    author: "By Emily Fields"
+                },
+                {
+                    dateTop: "07",
+                    dateBottom: "MAY '21",
+                    url: "#",
+                    imgUrl: "img/h-2-blog-img-3.jpg",
+                    title: "Introduce Richard Laperrière of those amazing features",
+                    author: "By Emily Fields"
+                },
+            ],
+            footerElements: [
+                {
+                    top: 'maree.qode@gmail.com',
+                    bottom: '+44645 321 789'
+                },
+                {
+                    top: 'Avenue d’Auderghem 10',
+                    bottom: '1040 Brussels, Belgium'
+                },
+            ],
             socialIcons: [
                 {
                     url: "https://twitter.com/",
@@ -48,20 +137,9 @@ var app = new Vue(
                     class: "fa-linkedin-in"
                 }
             ],
-            footerElements: [
-                {
-                    top: 'maree.qode@gmail.com',
-                    bottom: '+44645 321 789'
-                },
-                {
-                    top: 'Avenue d’Auderghem 10',
-                    bottom: '1040 Brussels, Belgium'
-                },
-            ]
         },
         methods: {
             scrollToTop: function() {
-                document.body.scrollTop = 0;
                 document.documentElement.scrollTop = 0;
             }
         },
